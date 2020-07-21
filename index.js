622d4e54017b9e17301965b283529076fe762b69
gastonkons <gastonkons@gmail.com>
2020-07-21 00:55:11 -0300

    upload the first files

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(cors());
+app.listen(port, ()=> { console.log(`server on ${port}`) });
+
+// JSON
+const about = require('./JSON/about.json');
+const portfolio = require('./JSON/portfolio.json');
+
+// Send json
+app.get('/about', (req, res) => { res.json(about) });
+app.get('/portfolio', (req, res) => { res.json(portfolio) })
